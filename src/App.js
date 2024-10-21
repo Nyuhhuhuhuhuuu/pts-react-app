10aecdc8a4dec8003978463865bade17a1e9f4c0
Cinamon <huangfredlie04@gmail.com>
2024-10-22 00:57:45 +0700

    update route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
 import Navbar from "./atribute/navbar";
 import Signup from "./atribute/signup";
 import Login from "./atribute/login";
-import { useState } from "react";
 
 export default function App() {
-  const [pages, setPages] = useState("Home");
-
-  const renderPages = () => {
-    if (pages === "login") {
-      return <Login />;
-    } else if (pages === "signup") {
-      return <Signup />;
-    }
-  };
   return (
     <div>
-      <Navbar setPages={setPages} />
-      <div>{renderPages()}</div>
+      <BrowserRouter>
+        <Navbar path="/" />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/atribute/navbar.js
+++ src/atribute/navbar.js
@@ -1,26 +1,29 @@
+import { Link } from "react-router-dom";
+
 import React from "react";
 
 export default function Navbar({ setPages }) {
   return (
     <div className="flex z-10 top-0 flex-items justify-between items-center py-4 px-6 bg-dark-teal sticky">
       <div className="flex items-center flex-shrink-0 text-slate-300 mr-6">
-        <img src="#" alt="logo" className="mr-2" />
-        <span className="font-semibold text-xl tracking-tight">Nama</span>
+        <Link to="/">
+          <img src="#" alt="logo" className="mr-2" />
+          <span className="font-semibold text-xl tracking-tight">Nama</span>
+        </Link>
       </div>
       <div className="flex flex-items items-center">
-        <button
-          className="text-lg mx-2 text-slate-300 hover:text-white transition-color ease-in-out duration-200"
-          onClick={() => setPages("login")}
-        >
-          Login
-        </button>
+        <Link to="/login">
+          <button className="text-lg mx-2 text-slate-300 hover:text-white transition-color ease-in-out duration-200">
+            Login
+          </button>
+        </Link>
         <span className="text-slate-300">|</span>
-        <button
-          className="text-lg mx-2 text-slate-300 hover:text-white transition-color ease-in-out duration-200"
-          onClick={() => setPages("signup")}
-        >
-          Sign Up
-        </button>
+
+        <Link to="/signup">
+          <button className="text-lg mx-2 text-slate-300 hover:text-white transition-color ease-in-out duration-200">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
